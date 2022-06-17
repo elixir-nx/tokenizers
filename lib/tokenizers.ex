f252fef922d93ca6f0d75bde71b35713896ce83c
José Valim <jose.valim@gmail.com>
2022-06-17 08:42:44 +0200

    Update tokenizers.ex

--- lib/tokenizers.ex
+++ lib/tokenizers.ex
@@ -8,8 +8,12 @@ defmodule Tokenizers do
   >
   > 🤗 Tokenizers provides an implementation of today’s most used tokenizers, with a focus on performance and versatility. These tokenizers are also used in 🤗 Transformers.
 
-  This library has bindings to use pretrained tokenizers. Support for building and training a tokenizer from scratch is forthcoming.
+  This library has bindings to use pretrained tokenizers. Support for building and training
+  a tokenizer from scratch is forthcoming.
 
-  A tokenizer is effectively a pipeline of transforms to take some input text and return a `Tokenizers.Encoding.t()`. The main entrypoint to this library is the `Tokenizers.Tokenizer` module, which holds the `Tokenizers.Tokenizer.t()` struct, a container holding the constituent parts of the pipeline. Most functionality is there.
+  A tokenizer is effectively a pipeline of transforms to take some input text and return a
+  `Tokenizers.Encoding.t()`. The main entrypoint to this library is the `Tokenizers.Tokenizer`
+  module, which holds the `Tokenizers.Tokenizer.t()` struct, a container holding the constituent
+  parts of the pipeline. Most functionality is there.
   """
 end
