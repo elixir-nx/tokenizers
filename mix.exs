25322072bf2326e3525ccf48aa0aed276235e21d
Erick Dennis <erick.dennis@gmail.com>
2023-02-21 13:09:54 +0100

    Allow usage of castore 1.0

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Tokenizers.MixProject do
 
   defp deps do
     [
-      {:castore, "~> 0.1"},
+      {:castore, "~> 0.1 or ~> 1.0"},
       {:ex_doc, "~> 0.28", only: :docs, runtime: false},
       {:rustler, ">= 0.0.0", optional: true},
       {:rustler_precompiled, "~> 0.5"}
