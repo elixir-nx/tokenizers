be0fc4ac1a9e820a0453728111b7f6a97e0e4354
Philip Sampaio <philip.sampaio@gmail.com>
2023-04-06 19:59:37 -0300

    Start dev version and update dev mix deps

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Tokenizers.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/elixir-nx/tokenizers"
-  @version "0.3.1"
+  @version "0.4.0-dev"
 
   def project do
     [
