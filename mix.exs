c676635c79b5be3cb330990dbb1a374f97d2dee6
Dmitry Rubinstein <dmitry@cyolo.io>
2023-07-13 16:15:57 +0300

    Reformat docs

--- mix.exs
+++ mix.exs
@@ -29,7 +29,6 @@ defmodule Tokenizers.MixProject do
 
   defp deps do
     [
-      {:dialyxir, "~> 1.3", only: [:test, :dev], runtime: false},
       {:castore, "~> 0.1 or ~> 1.0"},
       {:ex_doc, "~> 0.28", only: :docs, runtime: false},
       {:rustler, ">= 0.0.0", optional: true},
@@ -42,33 +41,7 @@ defmodule Tokenizers.MixProject do
       main: "Tokenizers",
       source_ref: "v#{@version}",
       source_url: @source_url,
-      extras: ["LICENSE", "notebooks/quicktour.livemd", "notebooks/pretrained.livemd"],
-      groups_for_docs: [
-        Creation: &(&1[:section] == :creators),
-        Setup: &(&1[:section] == :setup),
-        Inference: &(&1[:section] == :infer),
-        Training: &(&1[:section] == :train)
-      ],
-      groups_for_modules: [
-        Tokenization: [
-          Tokenizers.Tokenizer,
-          Tokenizers.Encoding
-        ],
-        Components: [
-          Tokenizers.Decoder,
-          Tokenizers.Normalizer,
-          Tokenizers.PreTokenizer,
-          Tokenizers.PostProcessor,
-          Tokenizers.Trainer,
-          Tokenizers.Model
-        ],
-        Models: [
-          Tokenizers.Model.BPE,
-          Tokenizers.Model.WordLevel,
-          Tokenizers.Model.WordPiece,
-          Tokenizers.Model.Unigram
-        ]
-      ]
+      extras: ["LICENSE", "notebooks/quicktour.livemd", "notebooks/pretrained.livemd"]
     ]
   end
 
